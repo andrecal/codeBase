97749a1db02e3258ee385d29ae5c17c1a63bc4c9
andrecal <caleb.andrews@uregina.ca>
2016-11-30 20:38:30 -0600

    coding
    yet to come

--- src/project/project.ino
+++ src/project/project.ino
@@ -16,35 +16,40 @@ const int tran3 = 8;
 //delay time
 int dT = 100;
 
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(tran1, OUTPUT);
   pinMode(tran2, OUTPUT);
   pinMode(tran3, OUTPUT);
+  pinMode(sen1, INPUT);
+  pinMode(sen2, INPUT);
+  pinMode(sen3, INPUT);
+
+  Serial.begin(9600);
   
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(sen1 == HIGH))
+  if(digitalRead(sen1) == HIGH)
   {
     digitalWrite(tran1, HIGH);
+    Serial.println("sen1");
     delay(dT);
     digitalWrite(tran1, LOW);
   }
-  if(digitalRead(sen2 == HIGH))
+  /*else if(digitalRead(sen2 == HIGH))
   {
     digitalWrite(tran2, HIGH);
     delay(dT);
     digitalWrite(tran2, LOW);
   }
-  if(digitalRead(sen3 == HIGH))
+  else if(digitalRead(sen3 == HIGH))
   {
     digitalWrite(tran3, HIGH);
     delay(dT);
     digitalWrite(tran3, LOW);
-  }
-
+  }*/
+ 
 }
